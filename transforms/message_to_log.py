a5b2f542bf3896167df42bf00a41592a1a2c33db
matthias-baetens <matthias.baetens@arabesque.com>
2022-10-21 13:58:01 +0800

    Update message fields

--- transforms/message_to_log.py
+++ transforms/message_to_log.py
@@ -10,7 +10,7 @@ class MessageToLog(beam.DoFn):
     def process(self, element, timestamp=beam.DoFn.TimestampParam):
         json_element = json.loads(element)
         yield Log(float(timestamp),
-                  json_element['body']['sentence'],
-                  json_element['host']
+                  json_element['text'],
+                  json_element['userId']
                   )
 
