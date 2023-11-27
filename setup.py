74cf6b69603d2733ab06f89b58b4ba0901790512
Matthias Baetens <baetensmatthias@gmail.com>
2023-11-27 17:24:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='beam-test',
+    name='beam-demo',
     version='1.0',
-    install_requires=['google-cloud-translate'],
     packages=find_packages(),
 )
